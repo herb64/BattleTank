ce5aaaed2ae48a1e5326c5cd1a4d6cd1bee42e55
Herbert Mehlhose <herbert.mehlhose@arcor.de>
2019-05-15 12:54:04 +0200

    Add force calculations

--- Source/BattleTank/Private/TankTrack.cpp
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -6,5 +6,19 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	UE_LOG(LogTemp, Warning, TEXT("%s: Throttle set: %f"), *GetName(), Throttle);
+	//UE_LOG(LogTemp, Warning, TEXT("%s: Throttle set: %f"), *GetName(), Throttle);
+
+	// Clamp forward vector, so that acceleration of tanks cannot be changed in editor
+	// by setting a higher scale value on the axis binding, which is used as Throttle.
+	FVector ForceApplied = GetForwardVector() * FMath::Clamp(Throttle, -1.0f, 1.0f) * MaxTrackForce;
+	FVector ForceLocation = GetComponentLocation();
+
+	// Owner is the tank. The root component is the upper component (a scene component), which
+	// here is the tank static mesh component.
+	// A scene component does not allow to apply any forces. Going down inheritance tree
+	// Primitive Component has this functionality, so we cast this
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+	UE_LOG(LogTemp, Warning, TEXT("%s: Apply force: %s"), *GetName(), *ForceApplied.ToString());
+
 }
--- Source/BattleTank/Public/TankTrack.h
+++ Source/BattleTank/Public/TankTrack.h
@@ -18,4 +18,14 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	// Maximum force in Newton that can be applied to the Track.
+	UPROPERTY(EditDefaultsOnly, Category = Driving, meta = (DisplayName = "Max Track Force", UIMin = "100000.0", UIMax = "2000000.0", ClampMin = "100000.0", ClampMax = "2000000.0"))
+	float MaxTrackForce = 400000.0f;
+
+	UPROPERTY(EditAnywhere, Category = Driving, meta = (DisplayName = "Dummy", UIMin = "100000.0", UIMax = "2000000.0", ClampMin = "100000.0", ClampMax = "2000000.0"))
+	float Dummy = 1000000.0f;
+
+	UPROPERTY(EditAnywhere, Category = Driving, meta = (DisplayName = "Dummy", UIMin = "100000.0", UIMax = "2000000.0", ClampMin = "100000.0", ClampMax = "2000000.0"))
+	float Dummy2 = 1100000.0f;
 };
